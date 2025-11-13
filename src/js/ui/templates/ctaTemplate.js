696a3b509a18fa606ab984de8c5c0386f98540c6
dashhhhk <daryachepaa@gmail.com>
2025-11-14 02:32:26 +0500

    Update ctaTemplate.js

--- src/js/ui/templates/ctaTemplate.js
+++ src/js/ui/templates/ctaTemplate.js
@@ -1,15 +1,48 @@
-export const ctaTemplate = (data) => {
-    return `
+export const Title = (title) => {
+  return `
+    <p class="cta-text-1">
+      ${title}
+    </p>
+  `;
+};
+
+export const Description = (description) => {
+  return `
+    <p class="cta-text-2">
+      ${description}
+    </p>
+  `;
+};
+
+export const ButtonText = (buttonText) => {
+  return `
+    <button class="cta-start">
+      ${buttonText}
+    </button>
+  `;
+};
+export const ctaTemplate = ({
+  title,
+  description,
+  buttonText,
+}) => {
+  const titleTemplate = Title(title);
+  const descriptionTemplate = Description(description);
+  const ButtonTextTemplate = ButtonText(buttonText);
+
+  const resultTemplate = `
         <section class="CTA">
             <div class="container">
                 <div class="cta-background">
-                    <p class="cta-text-1">${data.title}</p> 
-                    <p class="cta-text-2">${data.description}</p>
+                    ${titleTemplate}
+                    ${descriptionTemplate}
                     <div class="btn-cta">
-                        <button class="cta-start">${data.buttonText}</button>
+                        ${ButtonTextTemplate}
                     </div>
                 </div>
             </div>
         </section>
     `;
+
+  return resultTemplate;
 };
